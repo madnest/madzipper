0542fb7b69dd37804738f60cd3a90794854ee71d
Jakub Theimer <theimer@madne.st>
2021-11-20 18:10:16 +0100

    add commented out debug helper

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Madnest\Madzipper\Tests;
 
 use Madnest\Madzipper\MadzipperServiceProvider;
+// use Mockery;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
@@ -10,6 +11,9 @@ abstract class TestCase extends Orchestra
     public function setUp(): void
     {
         parent::setUp();
+
+        // Debug helper
+        // Mockery::setLoader(new Mockery\Loader\RequireLoader(sys_get_temp_dir()));
     }
 
     /**
