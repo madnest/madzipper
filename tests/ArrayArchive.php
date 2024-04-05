2ee525136ec13a8f18c6d7530a1bf642ef9e8575
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-04-05 18:38:25 +0200

    add types

--- tests/ArrayArchive.php
+++ tests/ArrayArchive.php
@@ -13,7 +13,7 @@ class ArrayArchive implements RepositoryInterface
      *
      * @param  bool  $new
      */
-    public function __construct($filePath, $new = false, $archiveImplementation = null)
+    public function __construct(string $filePath, bool $create = false, mixed $archive = null)
     {
     }
 
@@ -36,7 +36,7 @@ class ArrayArchive implements RepositoryInterface
     /**
      * Add a file to the opened Archive.
      */
-    public function addFile($pathToFile, $pathInArchive): void
+    public function addFile(string $pathToFile, string $pathInArchive): void
     {
         $this->entries[$pathInArchive] = $pathInArchive;
     }
@@ -44,7 +44,7 @@ class ArrayArchive implements RepositoryInterface
     /**
      * Add a file to the opened Archive using its contents.
      */
-    public function addFromString($name, $content): void
+    public function addFromString(string $name, string $content): void
     {
         $this->entries[$name] = $name;
     }
@@ -52,7 +52,7 @@ class ArrayArchive implements RepositoryInterface
     /**
      * Remove a file permanently from the Archive.
      */
-    public function removeFile($pathInArchive): void
+    public function removeFile(string $pathInArchive): void
     {
         unset($this->entries[$pathInArchive]);
     }
@@ -60,18 +60,15 @@ class ArrayArchive implements RepositoryInterface
     /**
      * Get the content of a file.
      */
-    public function getFileContent($pathInArchive): string
+    public function getFileContent(string $pathInArchive): string|false
     {
         return $this->entries[$pathInArchive];
     }
 
     /**
      * Get the stream of a file.
-     *
-     *
-     * @return mixed
      */
-    public function getFileStream($pathInArchive)
+    public function getFileStream(string $pathInArchive): mixed
     {
         return $this->entries[$pathInArchive];
     }
@@ -80,7 +77,7 @@ class ArrayArchive implements RepositoryInterface
      * Will loop over every item in the archive and will execute the callback on them
      * Will provide the filename for every item.
      */
-    public function each($callback): void
+    public function each(callable $callback): void
     {
         foreach ($this->entries as $entry) {
             call_user_func_array($callback, [
@@ -92,7 +89,7 @@ class ArrayArchive implements RepositoryInterface
     /**
      * Checks whether the file is in the archive.
      */
-    public function fileExists($fileInArchive): bool
+    public function fileExists(string $fileInArchive): bool
     {
         return array_key_exists($fileInArchive, $this->entries);
     }
@@ -108,14 +105,15 @@ class ArrayArchive implements RepositoryInterface
     /**
      * Closes the archive and saves it.
      */
-    public function close(): void
+    public function close(): bool
     {
+        return true;
     }
 
     /**
      * Add an empty directory.
      */
-    public function addEmptyDir($dirName): void
+    public function addEmptyDir(string $dirName): void
     {
         // CODE...
     }
@@ -123,7 +121,7 @@ class ArrayArchive implements RepositoryInterface
     /**
      * Sets the password to be used for decompressing.
      */
-    public function usePassword($password): void
+    public function usePassword(string $password): bool
     {
         // CODE...
     }
